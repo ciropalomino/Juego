dfac5fbc7853b99672b68f63ddf77672483e85ed
ciropalomino <ciro.cisco@gmail.com>
2017-01-19 08:08:16 -0500

    Change-Id: I2aca2327384be3d67031ec6ad7f3a7fad50ba744

--- Juego/src/mapa/Mapa.java
+++ Juego/src/mapa/Mapa.java
@@ -1,6 +1,7 @@
 package mapa;
 
 import graficos.Pantalla;
+import mapa.cuadro.Cuadro;
 
 public abstract class Mapa {
 	protected int ancho;
@@ -31,12 +32,19 @@ public abstract class Mapa {
 
 	}
 
-	public void mostrar(int compensacionX, int compensacionY, Pantalla pantalla) {
+	public void mostrar(final int compensacionX, final int compensacionY, final Pantalla pantalla) {
 		int o = compensacionX >> 5;
 		int e = (compensacionX + pantalla.obtenAncho()) >> 5;
 		int n = compensacionY >> 5;
 		int s = (compensacionY + pantalla.obtenAlto()) >> 5;
 
+	}
+	
+	public Cuadro obtencuadro (final int x, final int y){
+		if(cuadros[x+y*ancho]==0){
+			return Cuadro.ASFALTO;
+		}
+		return null;
 	}
 
 }
